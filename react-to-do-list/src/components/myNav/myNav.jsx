f10fc0e512dcea08df5280063c4e0d1967078516
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2018-02-07 22:01:02 -0800

    nav component

---
+++ react-to-do-list/src/components/myNav/myNav.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class myNav extends Component {
+    render() {
+        return (
+                <h1>Hello World!</h1>
+        )
+    }
+}
+
+export default myNav;
